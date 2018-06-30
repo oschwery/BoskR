d8e9ffe69df4d6b80af6b6ff811929ba561ef46b
oschwery <oschwery@vols.utk.edu>
2018-06-29 20:40:52 -0400

    first commit

---
+++ R/PvalMetrics.R
@@ -0,0 +1,20 @@
+PvalMetrics <-
+function(empMetrics, simMetrics) {
+  # loop getting distributions for all sim trees
+  dists <- list()
+  targetmetrics <- c("Princ_Eigenv_St", "Asymmetry_St", "Peakedness1_St")  # CHANGE once you have likelihood implemented!
+  pval <- matrix(nrow=nrow(empMetrics$metrics), ncol=2*length(targetmetrics))
+  for (j in 1:nrow(empMetrics$metrics)) {
+    dist <- list()
+    for (i in 1:length(targetmetrics)) {
+      dist[[i]] <- ecdf(simMetrics[[j]]$metrics[, targetmetrics[i]])
+      pval[j, i] <- empMetrics[1]$metrics[j, targetmetrics[i]]
+      pval[j, i+length(targetmetrics)] <- dist[[i]](c(empMetrics[1]$metrics[j, targetmetrics[i]]))
+    }
+    names(dist) <- targetmetrics
+    dists[[j]] <- dist
+  }
+  colnames(pval)  <- c(targetmetrics, paste("p-Value ", targetmetrics, sep=""))
+  pvals <- list(ECDs=dists, pValues=pval)
+  pvals
+}
