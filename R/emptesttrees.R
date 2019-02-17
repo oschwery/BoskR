a8853bef2818e17bcc88a7d947bc07e0f730925b
oschwery <oschwery@vols.utk.edu>
2019-02-16 23:32:49 -0500

    add testdata to documentation

---
+++ R/emptesttrees.R
@@ -0,0 +1,16 @@
+#' Set of Test Trees for BoskR
+#'
+#' ADD DESCRIPTION HERE
+#' @docType data
+#'
+#' @usage data(epmtesttrees)
+#'
+#' @format A list of phylogenies (of class phy)
+#'
+#' @keywords datasets
+#'
+#' @references ADD THEM HERE
+#'
+#' @source ADD THEM HERE
+#'
+"emptesttrees"
