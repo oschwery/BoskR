08de27d4ac7547c3e79aa4356fd8a2b12c331000
oschwery <oschwery@vols.utk.edu>
2019-03-10 16:26:47 -0400

    attempt to plot combo and fixed double name of wrapper function

--- R/ScatterMetrics.R
+++ R/ScatterMetrics.R
@@ -13,7 +13,14 @@
 #' @import scales alpha
 
 
-ScatterMetricsPairs <- ScatterMetricsPair <- function(empMetrics, simMetrics, pair=1, skim=FALSE, colours=c("black", "red"), transparencyEmp=0.8 ,transparencySim=0.2, pttype=16, ptsize=1.5, plottitle=paste("Empirical vs. Simulated Metrics Set", pair, sep=" "), perspective=-230) {
+ScatterMetrics <- function(empMetrics, simMetrics, pair=1, skim=FALSE, combine=FALSE, colours=c("black", "red"), transparencyEmp=0.8 ,transparencySim=0.2, pttype=16, ptsize=1.5, plottitle=paste("Empirical vs. Simulated Metrics Set", pair, sep=" "), perspective=-230) {
+  if (combine == TRUE) {
+    simComb <- c()
+    for (i in 1:length(simMetrics)) {
+      simComb <- rbind(simComb, simMetrics[[i]]$metrics)
+    }
+    ScatterMetricsCombo <-
+  }
   if (skim == TRUE) {
     for (i in 1:nrow(empMetrics$metrics)) {
       ScatterMetricsPair(empMetrics, simMetrics, pair=i, colours=colours, transparencyEmp=transparencyEmp, transparencySim=transparencySim, pttype=pttype, ptsize=ptsize, plottitle=plottitle, perspective=perspective)
@@ -27,6 +34,10 @@ ScatterMetricsPairs <- ScatterMetricsPair <- function(empMetrics, simMetrics, pa
 
 ScatterMetricsPair <- function(empMetrics, simMetrics, pair=1, colours=c("black", "red"), transparencyEmp=0.8, transparencySim=0.2, pttype=16, ptsize=0.8, plottitle=paste("Empirical vs. Simulated Metrics Set", pair, sep=" "), perspective=-230) {
   scatterplot3d(c(empMetrics$metrics[pair, "Asymmetry_St"], simMetrics[[pair]]$metrics[, "Asymmetry_St"]), c(empMetrics$metrics[pair, "Peakedness_St"], simMetrics[[pair]]$metrics[, "Peakedness_St"]), c(empMetrics$metrics[pair, "Princ_Eigenv_St"], simMetrics[[pair]]$metrics[, "Princ_Eigenv_St"]), xlab="Skewness", ylab="Kurtosis", zlab="Princ. Eigenvalue", pch=pttype, cex.symbols=ptsize, highlight.3d=FALSE, color=c(alpha(colours[1], transparencyEmp), alpha(c(rep(colours[2], times=nrow(simMetrics[[pair]]$metrics))), transparencySim)), type="p", main=plottitle, angle=perspective)
+}
+
+ScatterMetricsCombo <- function(empMetrics, simComb, colours=c("black", "red"), transparencyEmp=0.8, transparencySim=0.2, pttype=16, ptsize=0.8, plottitle="Empirical vs. Simulated Metrics Sets Combined", perspective=-230) {
+  scatterplot3d(c(empMetrics$metrics[, "Asymmetry_St"], simComb[, "Asymmetry_St"]), c(empMetrics$metrics[, "Peakedness_St"], simComb[, "Peakedness_St"]), c(empMetrics$metrics[, "Princ_Eigenv_St"], simComb[, "Princ_Eigenv_St"]), xlab="Skewness", ylab="Kurtosis", zlab="Princ. Eigenvalue", pch=pttype, cex.symbols=ptsize, highlight.3d=FALSE, color=c(alpha(rep(colours[1], times=nrow(empMetrics$metrics)), transparencyEmp), alpha(c(rep(colours[2], times=nrow(simComb$metrics))), transparencySim)), type="p", main=plottitle, angle=perspective)
 }
 
 
