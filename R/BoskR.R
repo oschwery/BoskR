54e7ce129e57e2a5d6692d052d7dc62d8fa39d51
oschwery <oschwery@vols.utk.edu>
2019-03-12 15:18:46 -0400

    fixed keywords

--- R/BoskR.R
+++ R/BoskR.R
@@ -1,7 +1,7 @@
 #' BoskR - Assess Adequacy of Diversification Models Using Tree Shapes
 #' @details \packageIndices{BoskR}
 #' @references In prep.
-#' @keyword package
+#' @keywords package
 #' @seealso \code{\link{CombineTrees}}, \code{\link{GetMetricTreeSets}}, \code{\link{GetTreeMetrics}}, \code{\link{GetTreeParams}}, \code{\link{PvalMetrics}}, \code{\link{ScatterMetrics}}, \code{\link{TreeCorr}}, \code{\link{plotPvalMetricsCDF}}, \code{\link{plotPvalMetricsPDF}}
 #'
 #' \code{\link[RPANDA:RPANDA-package]{RPANDA}}
