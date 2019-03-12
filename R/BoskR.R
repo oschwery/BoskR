f8f8be0b0c71849540511d487b08899788f0453a
oschwery <oschwery@vols.utk.edu>
2019-03-12 15:15:57 -0400

    added more sections

---
+++ R/BoskR.R
@@ -0,0 +1,8 @@
+#' BoskR - Assess Adequacy of Diversification Models Using Tree Shapes
+#' @details \packageIndices{BoskR}
+#' @references In prep.
+#' @keyword package
+#' @seealso \code{\link{CombineTrees}}, \code{\link{GetMetricTreeSets}}, \code{\link{GetTreeMetrics}}, \code{\link{GetTreeParams}}, \code{\link{PvalMetrics}}, \code{\link{ScatterMetrics}}, \code{\link{TreeCorr}}, \code{\link{plotPvalMetricsCDF}}, \code{\link{plotPvalMetricsPDF}}
+#'
+#' \code{\link[RPANDA:RPANDA-package]{RPANDA}}
+"_PACKAGE"
