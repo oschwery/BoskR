333a42a0e0d8d46cc51a17e3399c59ce0798d242
oschwery <oschwery@vols.utk.edu>
2019-06-06 03:06:31 -0400

    missing comma in stringsplit

--- R/GetTreeParams.R
+++ R/GetTreeParams.R
@@ -213,7 +213,7 @@ TimeDepBD <- function(treeset, current_method_est) {
   }
   # fill result into matrix
   for (i in 1:length(treeset)) {
-    outmatrix[(i+(0*length(treeset))),] <- c(RPANDA_result[[i]]$model, paste(deparse(substitute(treeset)),i, sep=" "), paste("RPANDA fit_bd", "lambda", strsplit(x=current_method_est, split="_")[[1]][2], "mu", strsplit(x=current_method_est, split="_")[[1]][3] sep=" "), RPANDA_result[[i]]$lambda_par[1],  RPANDA_result[[i]]$mu_par[1], NA, NA, RPANDA_result[[i]]$lambda_par[2], RPANDA_result[[i]]$mu_par[2], RPANDA_result[[i]]$LH, RPANDA_result[[i]]$aicc)
+    outmatrix[(i+(0*length(treeset))),] <- c(RPANDA_result[[i]]$model, paste(deparse(substitute(treeset)),i, sep=" "), paste("RPANDA fit_bd", "lambda", strsplit(x=current_method_est, split="_")[[1]][2], "mu", strsplit(x=current_method_est, split="_")[[1]][3], sep=" "), RPANDA_result[[i]]$lambda_par[1],  RPANDA_result[[i]]$mu_par[1], NA, NA, RPANDA_result[[i]]$lambda_par[2], RPANDA_result[[i]]$mu_par[2], RPANDA_result[[i]]$LH, RPANDA_result[[i]]$aicc)
   }
   outframe <- as.data.frame(outmatrix, stringsAsFactors=FALSE)
   return(outframe=outframe)
