8870978eda4fc0c801a7be71cfc85cea16adca57
oschwery <oschwery@vols.utk.edu>
2022-12-01 14:11:19 -0500

    option to get pvals for all metric sets

--- R/PvalMetrics.R
+++ R/PvalMetrics.R
@@ -2,13 +2,13 @@
 #'
 #' Estimates p-values based on simulated and empirical distributions of tree metrics
 #'
-#' The function uses an Empirical Cumulative Distribution function to determine the area under the curve of the metric values of the simulated trees, to get to a p-value for the position of the metrics of the empirical tree on that distribution. The argument `metricset` allows to chose between: `"spectR"`- the standard (i.e. unnormalised) spectral densities, `"spectRnorm"` - the normalised spectral densities, `"classic"` - a couple of more 'conventional' measures of tree shape, being Colless index, Sackin index, number of cherries, number of pitchforks, average ladder size, and gamma statistic; finally `"nodibranch" - includes minimum, maximum, and median for both node ages and branch lengths respectively. For more information on the spectral densities, i.e. the Eigenvalues of the tree's modified graph Laplacian, see R package RPANDA and associated papers.
+#' The function uses an Empirical Cumulative Distribution function to determine the area under the curve of the metric values of the simulated trees, to get to a p-value for the position of the metrics of the empirical tree on that distribution. The argument `metricset` allows to chose between: `"spectR"`- the standard (i.e. unnormalised) spectral densities, `"spectRnorm"` - the normalised spectral densities, `"classic"` - a couple of more 'conventional' measures of tree shape, being Colless index, Sackin index, number of cherries, number of pitchforks, average ladder size, and gamma statistic; finally `"nodibranch" - includes minimum, maximum, and median for both node ages and branch lengths respectively; option `"All"` returns all sets. For more information on the spectral densities, i.e. the Eigenvalues of the tree's modified graph Laplacian, see R package RPANDA and associated papers.
 #'
 #' @param empMetrics Metrics of empirical tree or set of trees; output of `GetTreeMetrics` or formatted the same way.
 #' @param simMetrics Metrics of sets of simulated trees; output of `GetTreeMetrics` or formatted the same way.
 #' @param empirical_start Indicator whether empMetrics is based on empirical or simulated initial trees, default is `TRUE` (=empirical); mainly important for data format reasons.
 #' @param methodnr Integral specifying which method is used: 1: BD, 2: TimeD-BD, 3: DD; is only used if `empirical_start` is `TRUE`
-#' @param metricset String specifying which tree metrics to use; default is "spectR", other options are "spectrRnorm", "classic", and "nodibranch"; for more information on the options see Details.
+#' @param metricset String specifying which tree metrics to use; default is "spectR", other options are "spectrRnorm", "classic", and "nodibranch", or "ALL"; for more information on the options see Details.
 #' @return A list with two entries: `ECDs` is a list of Empirical Cumulative Distributions; `pValues` is a matrix with p-values for the targeted metrics
 #'
 #' @export
@@ -18,13 +18,15 @@ PvalMetrics <- function(empMetrics, simMetrics, empirical_start=TRUE, methodnr, 
   dists <- list()  # primer for ECDs
   targetmetrics <-c()
   if (metricset == "spectR") {
-    targetmetrics <- c("Princ_Eigenv_St", "Asymmetry_St", "Peakedness_St")  # CHANGE once you have likelihood implemented!
+    targetmetrics <- c("Princ_Eigenv_St", "Asymmetry_St", "Peakedness_St", "Eigengap_St")  # CHANGE once you have likelihood implemented!
   } else if (metricset == "spectRnorm") {
     targetmetrics <- c("Princ_Eigenv_Nor", "Asymmetry_Nor", "Peakedness_Nor")
   } else if (metricset == "classic") {
     targetmetrics <- c("Colless", "Sackin", "Cherries", "pitchforks", "AvgLadder", "Gamma")
   } else if (metricset == "nodibranch") {
     targetmetrics <- c("Min_NodeAge", "Median_NodeAge", "Max_NodeAge", "Min_BranchLength", "Median_BranchLength", "Max_BranchLength")
+  } else if (metricset == "All") {
+    targetmetrics <- c("Colless", "Sackin", "Cherries", "pitchforks", "AvgLadder", "Gamma", "Min_NodeAge", "Median_NodeAge", "Max_NodeAge", "Min_BranchLength", "Median_BranchLength", "Max_BranchLength", "Princ_Eigenv_St", "Asymmetry_St", "Peakedness_St", "Eigengap_St", "Princ_Eigenv_Nor", "Asymmetry_Nor", "Peakedness_Nor")
   }
   
   Methods <- c("BD", "TimeD-BD", "DD")
